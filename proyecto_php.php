293d125ce73032f8c340e218416fbcec2dec3141
GerardoM3 <gerardomonroy13@gmail.com>
2022-04-28 22:55:29 -0600

    Segundo commit utilizando la terminal Git Bash

--- proyecto_php.php
+++ proyecto_php.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Git Bash</title>
 </head>
 <body>
-
+	<h1>¡Hola Mundo!</h1>
+	<p>He utilizado Git bash para subir este archivo PHP.</p>
+	<p>Este archivo es commiteado sin estilo</p>
 </body>
 </html>
