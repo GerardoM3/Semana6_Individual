e7e6d194c1625bf88b2f226c75a21d15102a8800
GerardoM3 <gerardomonroy13@gmail.com>
2022-04-28 23:00:53 -0600

    Tercer commit utilizando la terminal Git Bash

--- proyecto_php.php
+++ proyecto_php.php
@@ -2,10 +2,28 @@
 <html>
 <head>
 	<title>Git Bash</title>
+	<style type="text/css">
+		body{
+			background-color: orange;
+		}
+		h1{
+			background-color: darkblue;
+			color: white;
+			margin: 10px;
+		}
+		div.parrafo{
+			background-color: gray;
+			margin: 20px;
+		}
+	</style>
 </head>
 <body>
-	<h1>¡Hola Mundo!</h1>
-	<p>He utilizado Git bash para subir este archivo PHP.</p>
-	<p>Este archivo es commiteado sin estilo</p>
+	<center>
+		<h1>¡Hola Mundo!</h1>
+		<div class="parrafo">
+			<p>He utilizado Git bash para subir este archivo PHP.</p>
+			<p>Este archivo es commiteado sin estilo</p>
+		</div>
+	</center>
 </body>
 </html>
