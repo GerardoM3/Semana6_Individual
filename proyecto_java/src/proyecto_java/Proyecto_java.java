02958dcb1dbd2466fcd9b9e06a31114c6fcccf6d
GerardoM3 <gerardomonroy13@gmail.com>
2022-04-29 23:15:15 -0600

    Tercer commit utilizando Sourcetree con Java

--- proyecto_java/src/proyecto_java/Proyecto_java.java
+++ proyecto_java/src/proyecto_java/Proyecto_java.java
@@ -1,15 +1,29 @@
 
 package proyecto_java;
 
+import java.util.Scanner;
+
 public class Proyecto_java {
 
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         double a, b, suma;
         a = 3;
         b = 7;
         suma = a + b;
         System.out.println("Hola Mundo!");
         System.out.println(a + " + " + b + " = " + suma);
+        System.out.println("Ingrese primer número:");
+        a = Double.parseDouble(leer.nextLine());
+        System.out.println("Ingrese segundo número:");
+        b = Double.parseDouble(leer.nextLine());
+        restar(a, b);
+    }
+    
+    public static double restar(double a, double b){
+        double resultado;
+        resultado = a - b;
+        return resultado;
     }
     
 }
