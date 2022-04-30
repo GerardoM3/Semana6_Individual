31ff1ff52c26a0e1a63d43f8e8aebd37257bd2aa
GerardoM3 <gerardomonroy13@gmail.com>
2022-04-29 23:08:04 -0600

    Segundo commit utilizando Sourcetree con Java

--- proyecto_java/src/proyecto_java/Proyecto_java.java
+++ proyecto_java/src/proyecto_java/Proyecto_java.java
@@ -4,7 +4,12 @@ package proyecto_java;
 public class Proyecto_java {
 
     public static void main(String[] args) {
+        double a, b, suma;
+        a = 3;
+        b = 7;
+        suma = a + b;
         System.out.println("Hola Mundo!");
+        System.out.println(a + " + " + b + " = " + suma);
     }
     
 }
